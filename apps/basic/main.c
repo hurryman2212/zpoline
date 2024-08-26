ceef40f9dfb643820a934470660be938010f97d5
Jihong Min <hurryman2212@gmail.com>
2024-08-26 13:10:13 +0900

    Modify logging style

--- apps/basic/main.c
+++ apps/basic/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#include <unistd.h>
+
+#include <libaudit.h>
+
 typedef long (*syscall_fn_t)(long, long, long, long, long, long, long);
 
 static syscall_fn_t next_sys_call = NULL;
@@ -8,15 +12,13 @@ static long hook_function(long a1, long a2, long a3,
 			  long a4, long a5, long a6,
 			  long a7)
 {
-	printf("output from hook_function: syscall number %ld\n", a1);
+	fprintf(stderr, "[tid %d] %s(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)\n", gettid(), audit_syscall_to_name(a1, MACH_86_64), a2, a3, a4, a5, a6, a7);
 	return next_sys_call(a1, a2, a3, a4, a5, a6, a7);
 }
 
 int __hook_init(long placeholder __attribute__((unused)),
 		void *sys_call_hook_ptr)
 {
-	printf("output from __hook_init: we can do some init work here\n");
-
 	next_sys_call = *((syscall_fn_t *) sys_call_hook_ptr);
 	*((syscall_fn_t *) sys_call_hook_ptr) = hook_function;
 
